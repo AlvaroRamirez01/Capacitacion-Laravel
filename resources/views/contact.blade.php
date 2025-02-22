0f2b4eb7eecd6b7998b514f95b4e81d3a1c01bbd
Alvaro Ramirez(ASRock-Ryzen) <alvaro-rl@outlook.com>
2025-02-21 23:36:14 -0600

    new: created layouts and components files with blade and php

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,11 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Contact route</title>
-</head>
-<body>
-    <h1>This is the Contact page.</h1>
-</body>
-</html>
+<x-layout>
+    <h1>Hello from the Contact Page.</h1>
+</x-layout>
